223d84c88bc91d9dd63e57926b23eac86dcaa8e0
PURPUSHECHKA <purpushechka@gmail.com>
2021-05-02 03:38:19 +0500

    added sorting logic

--- client/redux/reducers/reducerOfGoods.js
+++ client/redux/reducers/reducerOfGoods.js
@@ -4,6 +4,7 @@ const GET_GOODS = 'GET_GOODS'
 const GET_RATES = 'GET_RATES'
 const SET_CURRENCY_RATE = 'SET_CURRENCY_RATE'
 const GET_SEARCH_CHARACTER_TO_FILTER = 'GET_SEARCH_CHARACTER_TO_FILTER'
+const SORTED_GOODS = 'SORTED_GOODS'
 
 const initialState = {
   listOfGoods: [],
@@ -12,8 +13,6 @@ const initialState = {
   currencyType: 'USD',
   charForFilter: ''
 }
-
-
 
 export default (state = initialState, action) => {
   switch (action.type) {
@@ -24,6 +23,14 @@ export default (state = initialState, action) => {
         listOfGoods
       }
     }
+    case SORTED_GOODS: {
+      const { sortedListOfGoods } = action
+      return {
+        ...state,
+        listOfGoods: sortedListOfGoods
+      }
+    }
+
     case GET_RATES: {
       const { rates } = action
       return {
@@ -40,7 +47,7 @@ export default (state = initialState, action) => {
       }
     }
     case GET_SEARCH_CHARACTER_TO_FILTER: {
-      const {char} = action
+      const { char } = action
       return {
         ...state,
         charForFilter: char
@@ -66,7 +73,6 @@ export const gettingRates = () => {
   return async (dispatch) => {
     try {
       const { data: rates } = await axios('/api/v1/rates')
-      console.log(rates)
       dispatch({ type: GET_RATES, rates })
     } catch (err) {
       dispatch({ type: GET_RATES, rates: {} })
@@ -75,12 +81,13 @@ export const gettingRates = () => {
 }
 
 export const setCurrencyRate = (currencyType) => {
-  return  (dispatch, getState) => {
+  return (dispatch, getState) => {
     const store = getState()
-    const { rates } =  store.reducerOfGoods
+    const { rates } = store.reducerOfGoods
     dispatch({
       type: SET_CURRENCY_RATE,
-      currencyRate: rates[currencyType], currencyType
+      currencyRate: rates[currencyType],
+      currencyType
     })
   }
 }
@@ -88,5 +95,20 @@ export const setCurrencyRate = (currencyType) => {
 export const getCharactersToFilter = (char) => {
   return (dispatch) => {
     dispatch({ type: GET_SEARCH_CHARACTER_TO_FILTER, char })
+  }
+}
+
+export const doSortingGoods = (typeOfSort) => {
+  return (dispatch, getState) => {
+    const store = getState()
+    const { listOfGoods } = store.reducerOfGoods
+    const parsedTypeOfSort = JSON.parse(typeOfSort)
+    const {type, order} = parsedTypeOfSort
+    const sortedListOfGoods = [...listOfGoods].sort((a, b) => {
+      return type === 'price'
+        ? (a.price - b.price) * order
+        : a.title.localeCompare(b.title) * order
+    })
+    dispatch({ type: SORTED_GOODS, sortedListOfGoods })
   }
 }
