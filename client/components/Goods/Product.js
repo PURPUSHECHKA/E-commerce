7793008d9017568fb78ae9ec47efb52a6d7d29ee
PURPUSHECHKA <purpushechka@gmail.com>
2021-05-03 02:59:14 +0500

    added responsive Product v1.0

--- client/components/Goods/Product.js
+++ client/components/Goods/Product.js
@@ -5,13 +5,11 @@ import PropTypes from 'prop-types'
 const Product = ({ allGoods: { image, title, price } }) => {
   const { currencyRate, currencyType } = useSelector((s) => s.reducerOfGoods)
   return (
-    <section className="flex flex-col rounded-md shadow-2xl bg-lime-100">
-      <img
-        alt={title}
-        className="block object-cover w-full rounded-t-md mb-2"
-        src={image}
-      />
-      <span className="h-16 text-center leading-2 text-black md:text-2xl text-xl mb-1">{title}</span>
+    <section className="max-w-240 mx-auto flex flex-col rounded-lg shadow-2xl bg-lime-100">
+      <img alt={title} className="block object-cover h-48 w-full rounded-t-lg mb-2" src={image} />
+      <span className="h-16 text-center leading-2 text-black lg:text-2xl text-xl mb-1">
+        {title}
+      </span>
       <div className="flex flex-col">
         <span className="text-center text-xl mb-2">
           {(price * currencyRate).toFixed(2)} {currencyType}
@@ -20,7 +18,8 @@ const Product = ({ allGoods: { image, title, price } }) => {
           id="cart button"
           type="button"
           className="text-xl flex justify-center bg-teal-500 hover:bg-teal-400 rounded-full shadow-lg focus:outline-none"
-        >      Add to cart
+        >
+          Add to cart
         </button>
       </div>
     </section>
