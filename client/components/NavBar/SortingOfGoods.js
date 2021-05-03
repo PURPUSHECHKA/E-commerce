c8f17e321a7d3aa59cd0b23760bd5e9c0c219e64
PURPUSHECHKA <purpushechka@gmail.com>
2021-05-03 21:28:02 +0500

    fixed the location of the text in responsive design v1.1

--- client/components/NavBar/SortingOfGoods.js
+++ client/components/NavBar/SortingOfGoods.js
@@ -63,7 +63,7 @@ const SortingOfGoods = () => {
       {({ open }) => (
         <>
           <div>
-            <Menu.Button className="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 bg-white text-sm font-medium focus:outline-none">
+            <Menu.Button className="inline-flex justify-center w-full whitespace-nowrap rounded-md shadow-sm px-4 py-2 bg-white text-sm font-medium focus:outline-none">
               Filter by
               <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
             </Menu.Button>
