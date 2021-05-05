d447cfc05fb945053f722d57b3ff49bd0e756b71
PURPUSHECHKA <purpushechka@gmail.com>
2021-05-06 01:35:01 +0500

    updated ListOfGoods for created Basket

--- client/components/Goods/ListOfGoods.js
+++ client/components/Goods/ListOfGoods.js
@@ -10,6 +10,7 @@ import ProductNotFound from './ProductNotFound'
 const Goods = () => {
   const dispatch = useDispatch()
   const { listOfGoods, charForFilter } = useSelector((s) => s.reducerOfGoods)
+  const { listOfIdsProducts } = useSelector((s) => s.reducerOfBasket)
 
   const style = `mx-auto max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-5xl 2xl:max-w-7xl 3xl:max-w-9xl
    grid gap-4 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-6 xl:gap-8 2xl:grid-cols-5 2xl:gap-10 3xl:grid-cols-6 3xl:gap-12`
@@ -19,19 +20,34 @@ const Goods = () => {
     dispatch(gettingRates())
   }, [])
 
-  const renderGoods = listOfGoods.reduce((acc, allGoods) => {
-    if (charForFilter === '') {
-      return [...acc, <Product key={allGoods.id} allGoods={allGoods} />]
+  const renderGoods = listOfGoods.map((particularProduct) => {
+    const basketCount = listOfIdsProducts.find(({ id }) => {
+      return id === particularProduct.id
+    })
+    if (particularProduct.title.toLowerCase().includes(charForFilter.toLowerCase())) {
+      return (
+        <Product
+          basketCount={basketCount?.quantity}
+          key={particularProduct.id}
+          particularProduct={particularProduct}
+        />
+      )
     }
-    if (allGoods.title.toLowerCase().includes(charForFilter.toLowerCase())) {
-      return [...acc, <Product key={allGoods.id} allGoods={allGoods} />]
-    }
-    return acc
-  }, [])
-  return renderGoods.length < 1 ? (
-    <ProductNotFound />
-  ) : (
-    <article className={style}>{renderGoods}</article>
+    return (
+      charForFilter === '' && (
+        <Product
+          quantity={basketCount?.quantity}
+          key={particularProduct.id}
+          particularProduct={particularProduct}
+        />
+      )
+    )
+  })
+  console.log(renderGoods.length)
+  return (
+    (renderGoods.length && <article className={style}>{renderGoods}</article>) || (
+      <ProductNotFound />
+    )
   )
 }
 
