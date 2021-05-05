e8a564f147e28a7b68d31bd9b74dbd0594eec9d2
PURPUSHECHKA <purpushechka@gmail.com>
2021-05-06 01:38:41 +0500

    added reducer for basket

--- client/redux/reducers/index.js
+++ client/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 
 import reducerOfGoods from './reducerOfGoods'
+import reducerOfBasket from './reducerOfBasket'
 
 const createRootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
-    reducerOfGoods
+    reducerOfGoods,
+    reducerOfBasket
   })
 
 export default createRootReducer
