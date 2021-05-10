1f1c5f0ede9590116182dd38b78c3a81530cdf2b
PURPUSHECHKA <purpushechka@gmail.com>
2021-05-10 20:59:04 +0500

    added constant for number and added optimization

--- client/components/Goods/Product.js
+++ client/components/Goods/Product.js
@@ -1,22 +1,28 @@
-import React, { memo } from 'react'
+import React, { memo, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { addIdForProduct } from '../../redux/reducers/reducerOfBasket'
 import AddToBasket from '../Basket/AddToBasket'
+import { ADD_ONE_PRODUCT } from '../constantes/constantes'
 
 const Product = ({ particularProduct, basketCount }) => {
-  const { image, title, price, id } = particularProduct
   const dispatch = useDispatch()
   const { currencyRate, currencyType } = useSelector((s) => s.reducerOfGoods)
-  const addProduct = () => dispatch(addIdForProduct(id, 1))
+  const { image, title, price, id } = particularProduct
+  const costOfgoods = (price * currencyRate).toFixed(2)
+
+  const addProduct = useCallback(() => dispatch(addIdForProduct(id, ADD_ONE_PRODUCT)), [
+    id,
+    dispatch
+  ])
   return (
     <section className="max-w-240 mx-auto flex flex-col rounded-lg shadow-lg hover:shadow-2xl bg-lime-100">
       <img alt={title} className="block object-cover h-48 w-full rounded-t-lg mb-2" src={image} />
       <span className="h-16 text-center leading-2 text-black text-xl mb-1">{title}</span>
       <div className="flex flex-col">
         <span className="text-center text-xl mb-2">
-          {(price * currencyRate).toFixed(2)} {currencyType}
+          {costOfgoods} {currencyType}
         </span>
         {!basketCount && (
           <button
@@ -36,12 +42,19 @@ const Product = ({ particularProduct, basketCount }) => {
   )
 }
 
-Product.ropTypes = {
-  id: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  image: PropTypes.string.isRequired,
-  quantity: PropTypes.number
+Product.propTypes = {
+  particularProduct: PropTypes.PropTypes.shape({
+    id: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.number,
+    title: PropTypes.string,
+    description: PropTypes.string
+  }).isRequired,
+  basketCount: PropTypes.number
+}
+
+Product.defaultProps = {
+  basketCount: null
 }
 
 export default memo(Product)
