7fd3390fc34a1d70e254ee48d985fb653b4dc354
PURPUSHECHKA <purpushechka@gmail.com>
2021-05-10 20:43:13 +0500

    the logic is divided into components and functions

--- client/components/Basket/QuantityGoodsInBasket.js
+++ client/components/Basket/QuantityGoodsInBasket.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import GetTheSumOfAllProducts from '../reuseComponents/GetTheSumOfAllProducts'
 
 const CartIcon = () => {
   const { listOfIdsProducts } = useSelector((s) => s.reducerOfBasket)
-  const totalBasketCount = listOfIdsProducts.reduce((acc, { quantity }) => acc + quantity, 0)
+  const totalBasketCount = useMemo(
+    () => listOfIdsProducts.reduce((acc, { quantity }) => acc + quantity, 0),
+    [listOfIdsProducts]
+  )
+
   return (
-    <div className="relative flex items-center h-6 w-6 lg:h-8 lg:w-8">
-      <Link to="/basket">
-        <img
-          alt="basket"
-          src="https://img.icons8.com/pastel-glyph/64/4a90e2/shopping-basket-2--v1.png"
-        />
-      </Link>
-      <span className="absolute right-0 top-0 rounded-full bg-red-600 text-teal-100 text-xs px-1">
-        {totalBasketCount}
-      </span>
-    </div>
+    <>
+      <GetTheSumOfAllProducts />
+      <div className="relative flex items-center h-6 w-6 lg:h-8 lg:w-8">
+        <Link to="/basket">
+          <img
+            alt="basket"
+            src="https://img.icons8.com/pastel-glyph/64/4a90e2/shopping-basket-2--v1.png"
+          />
+        </Link>
+        <span className="absolute right-0 top-0 rounded-full bg-red-600 text-teal-100 text-xs px-1">
+          {totalBasketCount}
+        </span>
+      </div>
+    </>
   )
 }
 CartIcon.propTypes = {}
