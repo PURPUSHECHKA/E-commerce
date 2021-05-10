5997e2ac6500762e25cba8291806f0a902b10443
PURPUSHECHKA <purpushechka@gmail.com>
2021-05-10 20:39:34 +0500

    added constant for naming a  magic number and added optimization

--- client/components/Basket/AddToBasket.js
+++ client/components/Basket/AddToBasket.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useDispatch } from 'react-redux'
 import { addIdForProduct } from '../../redux/reducers/reducerOfBasket'
+import { ADD_ONE_PRODUCT, DELETE_ONE_PRODUCT } from '../constantes/constantes'
 
 const AddToBasket = ({ particularProduct, basketCount }) => {
   const dispatch = useDispatch()
-  const changeProductQuantity = (id, value) => dispatch(addIdForProduct(id, value))
+  const changeProductQuantity = useCallback((id, value) => dispatch(addIdForProduct(id, value)), [
+    dispatch
+  ])
 
   return (
     <div className="flex flex-row bg-gradient-to-r from-blue-500 rounded-full to-red-500 text-xl">
       <button
         type="button"
-        onClick={() => changeProductQuantity(particularProduct.id, 1)}
+        onClick={() => changeProductQuantity(particularProduct.id, ADD_ONE_PRODUCT)}
         className="w-full focus:outline-none"
       >
         +
@@ -20,7 +23,7 @@ const AddToBasket = ({ particularProduct, basketCount }) => {
       </div>
       <button
         type="button"
-        onClick={() => changeProductQuantity(particularProduct.id, -1)}
+        onClick={() => changeProductQuantity(particularProduct.id, DELETE_ONE_PRODUCT)}
         className="w-full focus:outline-none"
       >
         -
